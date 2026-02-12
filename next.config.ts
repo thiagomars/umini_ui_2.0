4e549f56c809e09e8218ed58bcd70eef808c35ef
Thiago <r15.thiagomarques@gmail.com>
2026-02-12 15:36:29 -0300

    aaa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    /* config options here */
+const nextConfig = {
+    output: 'export', // Se estiver usando exportação estática
+    images: {
+        unoptimized: true, // Adicione esta linha
+    },
+    // basePath: '/umini_ui', // Substitua pelo nome do seu repo
+    // assetPrefix: '/nome-do-repositorio/',
 };
 
 export default nextConfig;
