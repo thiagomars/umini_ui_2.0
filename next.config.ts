4434610c8e97b095c2d56a63149ec8f35382bbaf
Thiago <r15.thiagomarques@gmail.com>
2026-04-28 22:09:36 -0300

    chore: update start script and disable image optimization for standalone deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "standalone",
+    images: {
+        unoptimized: true,
+    },
     /* config options here */
     headers: async () => {
         return [
